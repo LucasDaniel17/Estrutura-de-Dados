47bc39e48b5f4b097fd2018273bce7e804b0dd4f
LucasDaniel17 <101567841+LucasDaniel17@users.noreply.github.com>
2022-04-16 19:28:12 -0300

    Add files via upload

---
+++ Agenda/src/agenda/Principal.java
@@ -0,0 +1,49 @@
+package agenda;
+
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		Servico s = new Servico();
+		
+		System.out.println("<AGENDA DE CONTATOS>");
+		
+		
+		System.out.println("\nADICIONAR CONTATOS:");
+		for(int cont = 0; cont < 3; cont ++) {
+			Contato contato = new Contato();
+			
+			System.out.println("\nDigite o nome: ");
+			contato.setNome(input.next());
+			
+			System.out.println("Digite o número: ");
+			contato.setNumero(input.next());
+			
+			s.adicionar(contato);
+		}
+		s.listar();
+		
+		System.out.println("\n\nPROCURAR CONTATO:");
+		System.out.println("Qual contato deseja buscar?");
+		String nomeBusca = input.next();
+		
+		s.procurar(nomeBusca);
+		
+		System.out.println("\n\nADICIONAR EM UMA POSIÇÃO ESPECÍFICA:");
+		Contato contatoAdd = new Contato();
+		
+		System.out.println("\nDigite a posição:");
+		int posicaoAdd = input.nextInt();
+		System.out.println("\nDigite o nome:");
+		contatoAdd.setNome(input.next());
+		System.out.println("\nDigite o número:");
+		contatoAdd.setNumero(input.next());
+		
+		s.adicionarPosicao(posicaoAdd, contatoAdd);
+		s.listar();
+		
+		input.close();
+	}
+}
