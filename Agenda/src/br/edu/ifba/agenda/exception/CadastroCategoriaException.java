48a1d37c14cfe31a2c19f8bf517ca0256ad7c09c
Lucas Daniel <101567841+lucasdanielzx@users.noreply.github.com>
2022-09-13 12:12:35 -0300

    Delete Agenda directory

--- Agenda/src/br/edu/ifba/agenda/exception/CadastroCategoriaException.java
+++
@@ -1,8 +0,0 @@
-package br.edu.ifba.agenda.exception;
-
-public class CadastroCategoriaException extends Exception{
-	
-	public CadastroCategoriaException(String nome) {
-		super("A categoria" + nome + " já existe!");
-	}
-}
